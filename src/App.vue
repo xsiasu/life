13107f902476aa0b6b0f023f70adaab56089773e
sgkim <sgkim@the-51.com>
2022-04-28 00:54:03 +0900

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,10 @@
     <v-card class="overflow-hidden">
       <v-app-bar
         absolute
-        color="#6A76AB"
         dark
-        shrink-on-scroll
-        prominent
-        src="https://picsum.photos/1920/1080?random"
-        fade-img-on-scroll
+        color="white"
         scroll-target="#scrolling-techniques-3"
       >
-        <template #img="{ props }">
-          <v-img
-            v-bind="props"
-            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-          />
-        </template>
         <router-link :to="{name:'home'}">
           <a>home</a>
         </router-link>
